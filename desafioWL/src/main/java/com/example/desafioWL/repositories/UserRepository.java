f3a05178fff2dc3a9f17f01fcd49d36f570df2c0
talessgomes <tales.gomes_sl@hotmail.com>
2022-06-10 16:45:56 -0300

    deploy

--- desafioWL/src/main/java/com/example/desafioWL/repositories/UserRepository.java
+++ desafioWL/src/main/java/com/example/desafioWL/repositories/UserRepository.java
@@ -1,12 +1,19 @@
 package com.example.desafioWL.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+//aqui é salvo os dados do banco 
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.desafioWL.entities.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 
 }
